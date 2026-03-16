4b377d97a095176eb5de32a92e253ce4187e64cb
Deon Du Preez <deondupreez3@gmail.com>
2026-03-17 01:01:14 +0200

    Only loading angularls in a directory that has both package.json and angular.json. Only loading ts_ls in a directory that has a package.json but no angular.json.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,17 +24,81 @@ return {
       "neovim/nvim-lspconfig",
       opts = {
         servers = {
-          -- TypeScript
-          ts_ls = {
-            root_dir = function(fname)
-              local util = require("lspconfig.util")
+          -- Angular
+          angularls = {
+            root_dir = function(bufnr, on_dir)
+              -- The project root is where the LSP can be started from
+              -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+              -- We select then from the project root, which is identified by the presence of a package
+              -- manager lock file.
+              local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+              -- Give the root markers equal priority by wrapping them in a table
+              root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+                or vim.list_extend(root_markers, { '.git' })
+              -- exclude deno
 
-              -- Don't start ts_ls if this file is inside an Angular project
-              if util.root_pattern("angular.json")(fname) then
-                return nil
+              local path = vim.api.nvim_buf_get_name(bufnr)
+              local project_root = vim.fs.root(path, root_markers)
+              if project_root == nil then
+                vim.notify("project_root not found, disabling angularls", vim.log.levels.INFO)
+                return
               end
 
-              return util.root_pattern("tsconfig.json", "package.json", ".git")(fname)
+              -- exclude non-angular projects
+              local angularjson_root = vim.fs.root(project_root, { 'angular.json' })
+              if angularjson_root == nil or (not project_root or #angularjson_root < #project_root) then
+                -- angular.json is further away from package manager lock, abort
+                vim.notify("angularjson_root found, disabling angularls: '" .. tostring(angularjson_root) .. "'", vim.log.levels.INFO)
+                return
+              end
+
+              on_dir(angularjson_root)
+            end,
+          },
+
+          -- TypeScript
+          ts_ls = {
+            root_dir = function(bufnr, on_dir)
+              -- The project root is where the LSP can be started from
+              -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+              -- We select then from the project root, which is identified by the presence of a package
+              -- manager lock file.
+              local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+              -- Give the root markers equal priority by wrapping them in a table
+              root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+                or vim.list_extend(root_markers, { '.git' })
+              -- exclude deno
+
+              local path = vim.api.nvim_buf_get_name(bufnr)
+              local project_root = vim.fs.root(path, root_markers)
+              if project_root == nil then
+                vim.notify("project_root not found, disabling ts_ls", vim.log.levels.INFO)
+                return
+              end
+
+              local deno_root = vim.fs.root(path, { 'deno.json', 'deno.jsonc' })
+              local deno_lock_root = vim.fs.root(path, { 'deno.lock' })
+              if deno_lock_root ~= nil and (not project_root or #deno_lock_root > #project_root) then
+                -- deno lock is closer than package manager lock, abort
+                vim.notify("deno lock is closer than package manager lock, abort, disabling ts_ls", vim.log.levels.INFO)
+                return
+              end
+
+              if deno_root ~= nil and (not project_root or #deno_root >= #project_root) then
+                -- deno config is closer than or equal to package manager lock, abort
+                vim.notify("deno config is closer than or equal to package manager lock, abort, disabling ts_ls", vim.log.levels.INFO)
+                return
+              end
+
+              -- exclude angular projects
+              local angularjson_root = vim.fs.root(path, { 'angular.json' })
+              if angularjson_root ~= nil and (not project_root or #angularjson_root >= #project_root) then
+                -- angular.json is closer than or equal to package manager lock, abort
+                vim.notify("angularjson_root found, disabling ts_ls", vim.log.levels.INFO)
+                return
+              end
+
+              on_dir(project_root)
             end,
           },
 
