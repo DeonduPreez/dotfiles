188047eeeb3db3f1eee69bbb3a48cf22543651d5
Deon Du Preez <deondupreez3@gmail.com>
2026-03-20 01:22:35 +0200

    Switched wezterm to powershell by default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,27 +1,30 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.keys = {
-    {
-        key = 'v',
-        mods = 'CTRL',
-        action = wezterm.action.PasteFrom 'Clipboard'
-    },
-    {
-        key = 'c',
-        mods = 'CTRL',
-        action = wezterm.action.CopyTo 'Clipboard'
-    },
-    {
-        key = 't',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.DisableDefaultAssignment
-    },
-    {
-        key = 'l',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.DisableDefaultAssignment
-    },
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	{
+		key = "t",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 }
 
+config.default_prog = { "powershell.exe", "-NoLogo" }
+
 return config
+
