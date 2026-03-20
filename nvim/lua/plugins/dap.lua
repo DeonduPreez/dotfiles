193f8a58354a1b3deff5c24e49259548e02b4870
Deon Du Preez <deondupreez3@gmail.com>
2026-03-20 21:18:22 +0200

    Completely broke netcore debugging in the name of improvements. It'll be good, I promise.

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -14,6 +14,8 @@ return {
 					require("dap").terminate()
 				end,
 				desc = "Terminate",
+				nowait = true,
+				remap = false,
 			},
 			{
 				"<leader>dr",
@@ -21,6 +23,8 @@ return {
 					require("dap").repl.open()
 				end,
 				desc = "Open REPL",
+				nowait = true,
+				remap = false,
 			},
 			{
 				"<leader>du",
@@ -28,6 +32,8 @@ return {
 					require("dapui").toggle()
 				end,
 				desc = "Toggle DAP UI",
+				nowait = true,
+				remap = false,
 			},
 			{
 				"<leader>de",
@@ -36,6 +42,26 @@ return {
 				end,
 				desc = "Eval Expression",
 				mode = { "n", "v" },
+				nowait = true,
+				remap = false,
+			},
+			{
+				"<leader>db",
+				function()
+					require("dap").list_breakpoints()
+				end,
+				desc = "List breakpoints",
+				nowait = true,
+				remap = false,
+			},
+			{
+				"<leader>de",
+				function()
+					require("dap").set_exception_breakpoints({ "all" })
+				end,
+				desc = "Set Exception breakpoints",
+				nowait = true,
+				remap = false,
 			},
 		},
 		config = function()
@@ -134,11 +160,38 @@ return {
 					type = "netcoredbg",
 					name = "Launch (Debug)",
 					request = "launch",
+					-- Run in a terminal (useful for apps with stdin/stdout)
+					-- "integratedTerminal" | "externalTerminal" | "none" (uses dap console)
+					-- console = "externalTerminal",
+					justMyCode = true, -- Default is true
+					env = {
+						ASPNETCORE_ENVIRONMENT = "Development",
+						-- ASPNETCORE_URLS = "http://localhost:5000",
+					},
+					cwd = function()
+						local configuration = "Debug"
+						local cwf = vim.api.nvim_buf_get_name(0):gsub("\\", "/")
+						local cwd = vim.fn.fnamemodify(cwf, ":p:h")
+
+						local project_setup = dotnet_helper.get_dotnet_project_setup(cwf, cwd, configuration)
+						if not project_setup then
+							vim.notify("Failed to get the working directory for the dll. Good luck :)")
+							return cwd
+						end
+
+						return project_setup.dll_root_dir
+					end,
 					program = function()
 						-- TODO : Parse the launchSettings.json and build a config for each profile
+						local wezterm = require("../helpers/wztrm-helper")
+
 						local configuration = "Debug"
-						local corenet_env = "Development"
-						local project_setup = dotnet_helper.get_dotnet_project_setup(configuration)
+
+						-- Get the current "working" file and current working directory
+						local cwf = vim.api.nvim_buf_get_name(0):gsub("\\", "/")
+						local cwd = vim.fn.fnamemodify(cwf, ":p:h")
+
+						local project_setup = dotnet_helper.get_dotnet_project_setup(cwf, cwd, configuration)
 
 						if not project_setup then
 							return nil
@@ -152,12 +205,10 @@ return {
 
 						vim.notify("Build Succeeded.", vim.log.levels.DEBUG)
 
-						-- Set the working directory to the highest_net_folder so we execute in that directory
-						vim.fn.chdir(project_setup.dll_root_dir)
+						wezterm.set_terminal_title(configuration .. " " .. project_setup.project_name)
 
 						-- TODO : We are keeping track of the old working directory and return to it when done with current debugging session.
 						-- Could get complex when running multiple configs at the same time. At that point, just disable the cwd change maybe?
-						vim.env.ASPNETCORE_ENVIRONMENT = corenet_env
 
 						return project_setup.dll_path
 					end,
@@ -169,8 +220,12 @@ return {
 					program = function()
 						-- TODO : Parse the launchSettings.json and build a config for each profile
 						local configuration = "Release"
+
+						local cwf = vim.api.nvim_buf_get_name(0):gsub("\\", "/")
+						local cwd = vim.fn.fnamemodify(cwf, ":p:h")
+
 						local corenet_env = "Production"
-						local project_setup = dotnet_helper.get_dotnet_project_setup(configuration)
+						local project_setup = dotnet_helper.get_dotnet_project_setup(cwf, cwd, configuration)
 
 						if not project_setup then
 							return nil
@@ -205,56 +260,90 @@ return {
 			-- ──────────────────────────────────────────────
 			-- TypeScript / js-debug-adapter
 			-- ──────────────────────────────────────────────
-			-- local js_debug_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter"
+			local js_debug_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter"
 
-			-- dap.adapters["pwa-node"] = {
-			-- type = "server",
-			-- host = "localhost",
-			-- port = "${port}",
-			-- executable = {
-			-- command = "node",
-			-- args = { js_debug_path, "${port}" },
-			-- },
-			-- }
+			local pwa_node_adapter = "pwa-node"
+
+			dap.adapters[pwa_node_adapter] = {
+				type = "server",
+				host = "localhost",
+				port = "${port}",
+				executable = {
+					command = "node",
+					args = { js_debug_path, "${port}" },
+				},
+			}
 
 			-- Vitest runs under Node, so we attach to the Node process
-			-- for _, lang in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
-			-- dap.configurations[lang] = {
-			-- {
-			-- type = "pwa-node",
-			-- request = "launch",
-			-- name = "Launch file (Node)",
-			-- program = "${file}",
-			-- cwd = "${workspaceFolder}",
-			-- sourceMaps = true,
-			-- resolveSourceMapLocations = {
-			-- "${workspaceFolder}/**",
-			-- "!**/node_modules/**",
-			-- },
-			-- },
-			-- {
-			-- type = "pwa-node",
-			-- request = "launch",
-			-- name = "Debug Vitest (current file)",
-			-- cwd = "${workspaceFolder}",
-			-- program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
-			-- args = { "run", "${file}" },
-			-- sourceMaps = true,
-			-- resolveSourceMapLocations = {
-			-- "${workspaceFolder}/**",
-			-- "!**/node_modules/**",
-			-- },
-			-- console = "integratedTerminal",
-			-- },
-			-- {
-			-- type = "pwa-node",
-			-- request = "attach",
-			-- name = "Attach to Node process",
-			-- processId = require("dap.utils").pick_process,
-			-- cwd = "${workspaceFolder}",
-			-- },
-			-- }
-			-- end
+			-- TODO : We need a system that can save configurations per project
+			-- TODO : Need to figure out if this runs every time we open a new project
+			for _, lang in ipairs({ "typescript", "javascript" }) do
+				vim.notify("We are here!", vim.log.levels.DEBUG)
+				dap.configurations[lang] = {
+					{
+						type = pwa_node_adapter,
+						request = "launch",
+						name = "Launch file (Node)",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+						sourceMaps = true,
+						resolveSourceMapLocations = {
+							"${workspaceFolder}/**",
+							"!**/node_modules/**",
+						},
+					},
+					{
+						type = pwa_node_adapter,
+						request = "launch",
+						name = "Debug Vitest (current file)",
+						cwd = "${workspaceFolder}",
+						program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
+						args = { "run", "${file}" },
+						sourceMaps = true,
+						resolveSourceMapLocations = {
+							"${workspaceFolder}/**",
+							"!**/node_modules/**",
+						},
+						console = "integratedTerminal",
+					},
+					{
+						type = pwa_node_adapter,
+						request = "attach",
+						name = "Attach to Node process",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-chrome",
+						request = "launch",
+						name = "Launch & Debug Chrome",
+						url = function()
+							local co = coroutine.running()
+							return coroutine.create(function()
+								vim.ui.input({
+									prompt = "Enter URL: ",
+									default = "http://localhost:3000",
+								}, function(url)
+									if url == nil or url == "" then
+										return
+									else
+										coroutine.resume(co, url)
+									end
+								end)
+							end)
+						end,
+						webRoot = vim.fn.getcwd(),
+						protocol = "inspector",
+						sourceMaps = true,
+						userDataDir = false,
+					},
+					{
+						name = "----- ↓ launch.json configs ↓ -----",
+						type = "",
+						request = "launch",
+					},
+				}
+			end
 		end,
 	},
 }
