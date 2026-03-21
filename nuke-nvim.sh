c60d3459a31b7ee3497c223dea5c99f1e540e173
Deon Du Preez <deondupreez3@gmail.com>
2026-03-21 14:05:33 +0200

    Added a prompt to check if we want to delete lazy-lock.json. Most of the time it'll be no so defaulting to that when pressing enter.

--- nuke-nvim.sh
+++ nuke-nvim.sh
@@ -1,4 +1,12 @@
-rm -f ~/.config/nvim/lazy-lock.json
+#!/usr/bin/env bash
+set -e
+
+read -rp "Also delete lazy-lock.json? [y/N] " answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+  rm -f ~/.config/nvim/lazy-lock.json
+  echo "Deleted lazy-lock.json"
+fi
+
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.cache/nvim
