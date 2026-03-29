a9d226ad7465b84d1aa3844a5c80bd69d151fae3
Deon <deondupreez3@gmail.com>
2026-03-29 15:53:10 +0200

    Changed default window separator to white from red.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -98,7 +98,7 @@ return {
 					PmenuThumb = { bg = colors.palette.sumiInk4 },
 
 					-- Vertical split separator: thin subtle line, not a thick bar.
-					WinSeparator = { fg = "red", bg = "NONE" },
+					WinSeparator = { fg = "#FFFFFF", bg = "NONE" },
 				}
 			end,
 		})
